4d3c7ec0dc626a0736d30367e9560e1e266e970c
Erhan YILDIRIM <60566498+terazihan@users.noreply.github.com>
2024-02-15 01:07:48 +0300

    Delete src directory

--- src/Archer.java
+++
@@ -1,5 +0,0 @@
-public class Archer extends GameChar{
-    public Archer(){
-        super(2,"Okcu",7,18,20);
-    }
-}
--- src/WarDiamond.java
+++
@@ -1,3 +0,0 @@
-public interface WarDiamond {
-    Object getWarDiamond();
-}
